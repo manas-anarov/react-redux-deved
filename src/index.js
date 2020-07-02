febcbdf543b8534b92c6094ef24aadb29502d19f
Your Name <you@example.com>
2020-07-02 18:04:07 +0600

    18min

--- src/index.js
+++ src/index.js
@@ -3,6 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
+
+
+
+const increment = () =>{
+	return{
+		type: 'INCREMENT'
+	}
+}
+
+const decrement = () =>{
+	return{
+		type: 'DECREMENT'
+	}
+}
+
+
+
+const counter = (state = 0, action) =>{
+	switch(action.type){
+		case "INCREMENT":
+			return state + 1;
+		case "DECREMENT":
+			return state - 1;	
+	}
+}
+
+let store = createStore(counter)
+
+
+store.subscribe(()=>console.log(store.getState()))
+
+store.dispatch(increment())
 
 ReactDOM.render(
   <React.StrictMode>
