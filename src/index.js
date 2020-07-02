df31bf1055f34f228e1b89ca8caf0ad6e8f6c01c
Your Name <you@example.com>
2020-07-02 19:42:39 +0600

    last version

--- src/index.js
+++ src/index.js
@@ -3,48 +3,75 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
 import {createStore} from 'redux';
+import allReducer from './reducers'
+import {Provider} from 'react-redux'
 
 
 
-const increment = () =>{
-	return{
-		type: 'INCREMENT'
-	}
-}
-
-const decrement = () =>{
-	return{
-		type: 'DECREMENT'
-	}
-}
+const myStore = createStore(
+	allReducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	)
 
 
 
-const counter = (state = 0, action) =>{
-	switch(action.type){
-		case "INCREMENT":
-			return state + 1;
-		case "DECREMENT":
-			return state - 1;	
-	}
-}
-
-let store = createStore(counter)
 
 
-store.subscribe(()=>console.log(store.getState()))
 
-store.dispatch(increment())
+// import {createStore} from 'redux';
+
+
+
+// const increment = () =>{
+// 	return{
+// 		type: 'INCREMENT'
+// 	}
+// }
+
+// const decrement = () =>{
+// 	return{
+// 		type: 'DECREMENT'
+// 	}
+// }
+
+
+
+// const counter = (state = 0, action) =>{
+// 	switch(action.type){
+// 		case "INCREMENT":
+// 			return state + 1;
+// 		case "DECREMENT":
+// 			return state - 1;	
+// 	}
+// }
+
+// let store = createStore(counter)
+
+
+// store.subscribe(()=>console.log(store.getState()))
+
+// store.dispatch(increment())
+// store.dispatch(increment())
+// store.dispatch(increment())
+
+
+
+
+
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
+  <Provider store={myStore}>
     <App />
+  </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
